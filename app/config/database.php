f27df07a8cb2966e8f2319f73a2d47bce9dcfd02
Ahmed Rizawan <ahm.rizawan@live.com>
2015-09-12 15:53:09 +0600

    Fainal Update sept 12

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'chaity_db',
-			'username'  => 'chaity_dbadmin',
-			'password'  => 'CAGyepG8LQ',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
